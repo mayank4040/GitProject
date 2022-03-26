b528f60a7e280ea45ab5873b864a6b3234cace90
mayank4040 <mayankkhandelwal4040@gmail.com>
2022-03-26 11:47:51 +0530

    commit4

--- Gitproject/src/org/comp/HelloWorld.java
+++ Gitproject/src/org/comp/HelloWorld.java
@@ -1,11 +1,97 @@
 package org.comp;
 
-public class HelloWorld {
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+class Employee {
+    String name;
+    int salary;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Mayank Changed this");
+    public Employee(){}
+    public Employee(String name,int salary)
+    {
+        this.name=name;
+        this.salary=salary;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void getSalary(int salary)
+    {
+        this.salary=salary;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getSalary()
+    {
+        return salary;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("<");
+        sb.append("name: ");
+        sb.append(name);
+        sb.append(" salary: ");
+        sb.append("" + salary+">");
+        return sb.toString();
 
-	}
+    }
+}
+class EmployeeInfo{
+    enum SortMethod {BYNAME, BYSALARY};
 
+    class compareName implements Comparator<Employee>
+    {
+        @Override
+        public int compare(Employee o1,Employee o2)
+        {
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+    class compareSalary implements Comparator<Employee>
+    {
+        @Override
+        public int compare(Employee o1,Employee o2)
+        {
+            return (o1.getSalary()-o2.getSalary());
+        }
+    }
+    public List<Employee> sort(List<Employee> emps, final SortMethod method)
+    {
+        if(method == SortMethod.BYSALARY)
+        {
+            return emps.stream().sorted(new compareSalary()).collect(Collectors.toList());
+        }
+        else
+        {
+            return emps.stream().sorted(new compareName()).collect(Collectors.toList());
+        }
+    }
+
+    public boolean isCharacterPresentInAllNames(Collection<Employee> entities, String character)
+    {
+        return entities.stream().anyMatch(m->m.getName().startsWith(character));
+        
+    }
+}
+
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        List<Employee> emps = new ArrayList<>();
+        emps.add(new Employee("Mickey", 1000));
+        emps.add(new Employee("Timmy", 50000));
+        emps.add(new Employee("Annny", 40000));
+
+        EmployeeInfo obj = new EmployeeInfo();
+        System.out.println(obj.sort(emps,EmployeeInfo.SortMethod.BYNAME));
+        System.out.println(obj.isCharacterPresentInAllNames(emps,"z"));
+        System.out.println(emps);
+    }
 }
